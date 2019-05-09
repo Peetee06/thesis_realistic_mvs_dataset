d744a7803efdaa66b2f2e60c7b41efd1eb66f29c
Peetee06 <peter_trost93@yahoo.de>
2019-05-09 17:46:54 +0200

    extract.py can now convert .rdc files to .png RGB images

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,111 +1,105 @@
-# RenderDoc Python console, powered by python 3.6.4.
-# The 'pyrenderdoc' object is the current CaptureContext instance.
-# The 'renderdoc' and 'qrenderdoc' modules are available.
-# Documentation is available: https://renderdoc.org/docs/python_api/index.html
-
-rd = renderdoc 
+# this script includes some code snippets from https://renderdoc.org/docs/python_api/examples/renderdoc/ by Baldur Karlsson from 2019
+# and playing-for-data https://bitbucket.org/visinf/projects-2016-playing-for-data/src/master/scripts/extract_from_game.py by Richter et al 2016
 
-# frameInfo = renderdoc.ReplayController.GetFrameInfo()
-fileName = "G:\\thesis_data\\renderdoc_files\\renderdoc_files-frame6481.rdc"
-# Open a capture file handle
-cap = rd.OpenCaptureFile()
-
-# Open a particular file - see also OpenBuffer to load from memory
-status = cap.OpenFile(fileName, '', None)
-
-# Make sure the file opened successfully
-if status != rd.ReplayStatus.Succeeded:
-    raise RuntimeError("Couldn't open file: " + str(status))
-
-# Make sure we can replay
-if not cap.LocalReplaySupport():
-    raise RuntimeError("Capture cannot be replayed")
-
-# Initialise the replay
-status,controller = cap.OpenCapture(None)
-
-if status != rd.ReplayStatus.Succeeded:
-    raise RuntimeError("Couldn't initialise replay: " + str(status))
-
-# Now we can use the controller!
-print("%d top-level drawcalls" % len(controller.GetDrawcalls()))
-
-frameInfo = controller.GetFrameInfo()
-frameId = frameInfo.frameNumber
-print(frameId)
-
-
-# TODO add reference to playing-for-data
-
-config = {}
-config['save_dir'] = 'G:\\thesis_data\\renderdoc_files\\processed' # where we store extraction results
-config['gbufferpass_colortargets'] = 4 # number of color targets that identify the G-buffer pass, most probably 4 as games want to use all available render targets
-config['gbufferpass_hasdepth']     = True # whether G-buffer pass has a depth target
-# Our names for the color buffers
-config['gbuffer_names']            = ['gbuffer1', 'gbuffer2', 'gbuffer3', 'gbuffer4'] # the number should match the config['gbufferpass_colortargets']
-
-# Get drawcalls
-drawcalls = controller.GetDrawcalls()
-print('Found %d drawcalls.' % len(drawcalls))
-
-def containsTargets(drawcallName, numColorTargets, hasDepthTarget):
-	""" Determines if drawcall has multiple render targets by checking its name. 
-	    The name is defined by renderdoc and contains information about render targets."""	
-	print("drawcall Name is: {}".format(drawcallName))
-	if hasDepthTarget:
-		return drawcallName.find('(%d Targets + Depth)' % numColorTargets) >= 0		
-	else:		
-		return drawcallName.find('(%d Targets)' % numColorTargets) >= 0		
-	pass
-
-def findGbufferPass(numColorTargets, hasDepthTarget):
-	""" Identifies the G-buffer pass. """
-	gbufferEnd = 0
-	gbufferIds = [i for i,call in enumerate(drawcalls) if containsTargets(call.name, numColorTargets, hasDepthTarget)]
-	print("there are %d gbuffer IDs" %len(gbufferIds))
-	if len(gbufferIds) == 1:
-		gbufferId    = gbufferIds[0]
-		gbufferCalls = drawcalls[gbufferId].children
-		print('G-buffer pass has %d drawcalls.' % len(gbufferCalls))
-		gbufferEnd   = drawcalls[gbufferId].children[-1].eventID # last drawcall of the G-buffer pass
-	
-	assert(gbufferEnd > 0, 'Did not find any drawcall with the specified G-buffer settings.')
-	return gbufferId, gbufferEnd
-	
-def findFinalPass(numColorTargets, hasDepthTarget, drawCallName):
-	""" Returns the EventID of the pass that draws the final image (before HUD). """
-
-	# Find last drawcall before HUD
-	potentialHudIds = [i for i,call in enumerate(drawcalls) if containsTargets(call.name, numColorTargets, hasDepthTarget)]
-	print('Found %d potential HUD passes.' % len(potentialHudIds))
-
-	colorpassIds = [i for i,call in enumerate(drawcalls) if containsTargets(call.name, 1, False)]
-
-	a = colorpassIds[:]
-	a.extend(potentialHudIds[:-3])
-	assert(len(a) > 0, 'Found not enough potential final passes.')
-	firstPotentialFinalId = max(a)
-
-	finalPassId = 0
-	return [call.eventID for call in drawcalls[-1:0:-1] if call.name.find(drawCallName) >= 0][1]
-	
-def getColorBuffers(frameId, eventId):
-	""" Sets the pipeline to eventId and returns the ids of bound render targets. """
-	renderdoc.SetEventID(None, frameId, eventId)
-	commonState   = renderdoc.CurPipelineState
-	outputTargets = commonState.GetOutputTargets()
-	return [t for t in outputTargets if str(t) != '0']
-
-gbufferId, gbufferEnd = findGbufferPass(config['gbufferpass_colortargets'], config['gbufferpass_hasdepth'])
-print('G-buffer pass is done at EID %d.' % gbufferEnd)
-bufferIds = getColorBuffers(frameId, gbufferEnd)
-
-# Save color targets
-for i,bid in enumerate(bufferIds):
- 	renderdoc.SaveTexture(bid, '{0}/{1}_{2}.png'.format(saveDir, "test", config['gbuffer_names'][i]))
-
-
-# Shutdown the controller first, then the capture file
-controller.Shutdown()
-
-cap.Shutdown()
+
+import renderdoc as rd
+import os
+
+
+config = {}
+config['file_dir'] = 'G:\\thesis_data\\renderdoc_files\\captures' # where .rdc capture files are stored
+config['save_dir'] = 'G:\\thesis_data\\renderdoc_files\\processed' # where we store extraction results
+
+# set up saving textures
+texsave = rd.TextureSave()
+
+# find depth pass
+def getDepthPass(drawcalls):
+    pass
+
+# find final drawcall before HUD is rendereddef getFinalPass(drawcalls):
+    foundFinalPass = False
+    for d in drawcalls[::-1]:
+        if foundFinalPass:            return d
+
+        if d.name.find("ClearRenderTargetView") >= 0:
+            foundFinalPass = True
+
+def saveDepth(controller, drawcalls, file_prefix):
+    filePath = '{0}\\{1}_depth.exr'.format(config['save_dir'], file_prefix)
+    fileName = filePath.split('\\')[-1]
+    if fileName not in os.listdir(config['save_dir']):
+        texsave.destType = rd.FileType.EXR
+        
+        depthPass = getDepthPass(drawcalls)
+        depthPassId = depthPass.eventId
+        
+        controller.SetFrameEvent(depthDraw, True)
+        texsave.resourceId = depthPass.depthOut
+        controller.SaveTexture(texsave, )    else:
+        print('depth-map of {} already exists and is therefore skipped'.format(file_prefix))
+
+def saveRGB(controller, drawcalls, file_prefix):
+    filePath = '{0}\\{1}_final.png'.format(config['save_dir'], file_prefix)
+    fileName = filePath.split('\\')[-1]
+    if fileName not in os.listdir(config['save_dir']):
+        print('filename {} is not yet in this directory'.format(fileName))
+        texsave.destType = rd.FileType.PNG
+    
+        finalPass = getFinalPass(drawcalls)
+        finalPassId = finalPass.eventId
+    
+        controller.SetFrameEvent(finalPassId, True)
+        texsave.resourceId = finalPass.outputs[0]
+        controller.SaveTexture(texsave, fileName)
+    else:
+        print('RGB image of {} already exists and is therefore skipped'.format(file_prefix))
+
+def saveCamParams(controller, drawcalls, file_prefix):
+    pass
+
+def process(fileName):
+    # Open a capture file handle
+    cap = rd.OpenCaptureFile()
+
+    # Open a particular file - see also OpenBuffer to load from memory
+    fullPath = config['file_dir'] + '\\' + fileName
+    status = cap.OpenFile(fullPath, '', None)
+
+    # Make sure the file opened successfully
+    if status != rd.ReplayStatus.Succeeded:
+        raise RuntimeError("Couldn't open file: " + str(status))
+
+    # Make sure we can replay
+    if not cap.LocalReplaySupport():
+        raise RuntimeError("Capture cannot be replayed")
+
+    # Initialise the replay
+    status,controller = cap.OpenCapture(None)
+
+    if status != rd.ReplayStatus.Succeeded:
+        raise RuntimeError("Couldn't initialise replay: " + str(status))
+
+    # Get drawcalls
+    drawcalls = controller.GetDrawcalls()
+
+    file_prefix = fileName[:-4] # remove .rdc ending
+    saveRGB(controller, drawcalls, file_prefix)
+    # saveDepth(controller, drawcalls, file_prefix)
+    # saveCamParams(controller, drawcalls, file_prefix) #TODO implement this function
+    
+    # Shutdown the controller first, then the capture file
+    controller.Shutdown()
+
+    cap.Shutdown()
+
+def main():
+    # todo skip files that have already been processed
+    for f in os.listdir(config['file_dir']):
+        print(f)
+        process(f)
+
+    print('finished processing all files in {}'.format(config['file_dir']))
+    print('processed files were saved to {}'.format(config['save_dir']))
+
+main()
