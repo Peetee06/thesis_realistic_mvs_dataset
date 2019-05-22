525153a48a533f09f2753d8900261edf9e5a72c4
Peetee06 <peter_trost93@yahoo.de>
2019-05-22 09:43:32 +0200

    changes in extract.py and added some notes

--- scripts/extract.py
+++ scripts/extract.py
@@ -7,15 +7,20 @@ import os
 
 
 config = {}
-config['file_dir'] = 'G:\\thesis_data\\renderdoc_files\\captures' # where .rdc capture files are stored
-config['save_dir'] = 'G:\\thesis_data\\renderdoc_files\\processed' # where we store extraction results
-
-# set up saving textures
-texsave = rd.TextureSave()
+config['file_dir'] = 'F:\\thesis\\renderdoc_files\\captures' # where .rdc capture files are stored
+config['save_dir'] = 'F:\\thesis\\renderdoc_files\\processed' # where we store extraction results
 
 # find depth pass
-def getDepthPass(drawcalls):
-    pass
+def getDepthPass(drawcalls):    
+    foundDepthPass = False
+    searchFromId = 1000 # adjust to the eventId around which the DepthPass starts
+    for d in drawcalls:
+        if (d.eventId > searchFromId) and (d.name.find("Dispatch") >= 0):
+            foundDepthPass = True
+        
+        if foundDepthPass:
+            print('found depthPass with eventId: {}'.format(d.previous.eventId))
+            return d.previous
 
 # find final drawcall before HUD is rendereddef getFinalPass(drawcalls):
     foundFinalPass = False
@@ -26,32 +31,46 @@ def getDepthPass(drawcalls):
             foundFinalPass = True
 
 def saveDepth(controller, drawcalls, file_prefix):
+    depthsave = rd.TextureSave()
+    print('saving depth')
     filePath = '{0}\\{1}_depth.exr'.format(config['save_dir'], file_prefix)
     fileName = filePath.split('\\')[-1]
     if fileName not in os.listdir(config['save_dir']):
-        texsave.destType = rd.FileType.EXR
+        depthsave.destType = rd.FileType.EXR
         
+        depthsave.channelExtract = 0
         depthPass = getDepthPass(drawcalls)
         depthPassId = depthPass.eventId
         
-        controller.SetFrameEvent(depthDraw, True)
-        texsave.resourceId = depthPass.depthOut
-        controller.SaveTexture(texsave, )    else:
+        controller.SetFrameEvent(depthPassId, True)
+        depthsave.resourceId = depthPass.depthOut
+        print("resourceId of depth texture is {}".format(depthsave.resourceId))
+        
+        # adjust white and black to min and max pixel values
+        depthOutput = controller.CreateOutput(rd.CreateHeadlessWindowingData(), rd.ReplayOutputType.Headless)
+        textureDisplay = rd.TextureDisplay()
+        textureDisplay.resourceId = depthPass.depthOut
+        depthOutput.SetTextureDisplay(textureDisplay)
+        min, max = depthOutput.GetMinMax()
+        
+        controller.SaveTexture(depthsave, filePath)
+        print('depth saved')    else:
         print('depth-map of {} already exists and is therefore skipped'.format(file_prefix))
 
 def saveRGB(controller, drawcalls, file_prefix):
+    RGBsave = rd.TextureSave()
     filePath = '{0}\\{1}_final.png'.format(config['save_dir'], file_prefix)
     fileName = filePath.split('\\')[-1]
     if fileName not in os.listdir(config['save_dir']):
         print('filename {} is not yet in this directory'.format(fileName))
-        texsave.destType = rd.FileType.PNG
+        RGBsave.destType = rd.FileType.PNG
     
         finalPass = getFinalPass(drawcalls)
         finalPassId = finalPass.eventId
     
         controller.SetFrameEvent(finalPassId, True)
-        texsave.resourceId = finalPass.outputs[0]
-        controller.SaveTexture(texsave, fileName)
+        RGBsave.resourceId = finalPass.outputs[0]
+        controller.SaveTexture(RGBsave, filePath)
     else:
         print('RGB image of {} already exists and is therefore skipped'.format(file_prefix))
 
@@ -82,10 +101,10 @@ def process(fileName):
 
     # Get drawcalls
     drawcalls = controller.GetDrawcalls()
-
+    
     file_prefix = fileName[:-4] # remove .rdc ending
     saveRGB(controller, drawcalls, file_prefix)
-    # saveDepth(controller, drawcalls, file_prefix)
+    saveDepth(controller, drawcalls, file_prefix)
     # saveCamParams(controller, drawcalls, file_prefix) #TODO implement this function
     
     # Shutdown the controller first, then the capture file
@@ -96,8 +115,9 @@ def process(fileName):
 def main():
     # todo skip files that have already been processed
     for f in os.listdir(config['file_dir']):
-        print(f)
+        print("processing {}".format(f))
         process(f)
+        break
 
     print('finished processing all files in {}'.format(config['file_dir']))
     print('processed files were saved to {}'.format(config['save_dir']))
